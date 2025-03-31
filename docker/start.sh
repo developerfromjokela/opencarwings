1e3aba218e73d8883f6cd8170e9a94cc015e8015
Developer From Jokela <developerfromjokela@gmail.com>
2025-03-31 16:25:27 +0300

    Add TCU server log

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,6 @@ cd /app || echo "Unable to navigate to /app"
 python manage.py migrate
 python manage.py collectstatic --noinput
 /usr/sbin/crond -f -l 8 &
-(python manage.py tcuserver 0.0.0.0; [ "$?" -lt 2 ] && kill "$$") &
+(python manage.py tcuserver 0.0.0.0 >> /app/logs/tcuserver.txt; [ "$?" -lt 2 ] && kill "$$") &
 (gunicorn --worker-class=gevent --worker-connections=$WORKER_CONNECTIONS --timeout $WORKER_TIMEOUT --access-logfile - --workers $WORKERS --bind 0.0.0.0:80 carwings.wsgi:application; [ "$?" -lt 2 ] && kill "$$") &
 wait

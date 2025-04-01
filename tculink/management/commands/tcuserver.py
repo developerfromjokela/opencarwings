0243e9992aae071aba27278f8f9464e380e3fe71
Developer From Jokela <developerfromjokela@gmail.com>
2025-04-01 19:24:22 +0300

    Log authentication check status

--- tculink/management/commands/tcuserver.py
+++ tculink/management/commands/tcuserver.py
@@ -134,6 +134,7 @@ class Command(BaseCommand):
 
                         # skip auth and set as authenticated if check is disabled
                         authenticated = car.disable_auth
+                        logger.info(f"TCU Authentication check status: {tcu_info}")
                         # auth before anything
                         if parsed_data["message_type"][0] != 5 and not authenticated:
                             auth_data = parsed_data.get("auth", None)

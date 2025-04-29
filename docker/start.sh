a4ad063bd2dffc7aa3819c0ca51c67ea29ff469a
Developer From Jokela <developerfromjokela@gmail.com>
2025-04-29 16:45:00 +0300

    Fix docker start

--- docker/start.sh
+++ docker/start.sh
@@ -7,5 +7,5 @@ python manage.py collectstatic --noinput
 /usr/sbin/crond -f -l 8 &
 (python manage.py tcuserver 0.0.0.0; [ "$?" -lt 2 ] && kill "$$") &
 #(gunicorn --worker-class=gevent --worker-connections=$WORKER_CONNECTIONS --timeout $WORKER_TIMEOUT --access-logfile - --workers $WORKERS --bind 0.0.0.0:80 carwings.wsgi:application; [ "$?" -lt 2 ] && kill "$$") &
-(daphne -b 0.0.0.0 -p 80 --access-log - "$@" carwings.asgi:channel_layer; [ "$?" -lt 2 ] && kill "$$") &
+(daphne -b 0.0.0.0 -p 80 --access-log - "$@" carwings.asgi:application; [ "$?" -lt 2 ] && kill "$$") &
 wait

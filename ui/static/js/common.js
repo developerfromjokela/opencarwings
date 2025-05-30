258237dda4d102e9e4b8411500946ab62300a887
Developer From Jokela <developerfromjokela@gmail.com>
2025-05-30 15:35:50 +0300

    Handle soft errors with title

--- ui/static/js/common.js
+++ ui/static/js/common.js
@@ -12,7 +12,14 @@ function showNotification({ message, title = '', type = 'success', persist_id=un
         case 'error':
             typeStyles = 'bg-red-100 text-red-800';
             iconPath = '<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />';
-            content = `<span>${message}</span>`;
+            if (title && title.length > 0) {
+                content = `<div class="flex flex-col"><span class="font-bold">${title}</span>`;
+                if (message)
+                    content += '<span>${message}</span>';
+                content += '</div>';
+            } else {
+                content = `<span>${message}</span>`;
+            }
             break;
         case 'info':
             typeStyles = 'bg-blue-100 text-blue-800';

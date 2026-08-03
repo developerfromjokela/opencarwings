19640fba2fe2293597cce4ca3b78efed47d8f05b
Developer From Jokela <developerfromjokela@gmail.com>
2026-08-03 15:01:59 +0300

    Disable checking DCM ID and ICCID, because on JDM models it causes issues.

--- tculink/carwings_proto/utils.py
+++ tculink/carwings_proto/utils.py
@@ -12,8 +12,7 @@ def get_cws_authenticated_car(xml_data, check_user=True) -> Car|None:
     if 'authentication' in xml_data:
 
         car_vin = xml_data['authentication']['vin']
-        dcm_id = xml_data['authentication']['dcm_id']
-        sim_id = xml_data['authentication']['sim_id']
+        navi_id = xml_data['authentication']['navi_id']
 
         username = xml_data['authentication']['user_id']
         password = xml_data['authentication']['password']
@@ -23,11 +22,7 @@ def get_cws_authenticated_car(xml_data, check_user=True) -> Car|None:
             car = Car.objects.get(vin=car_vin)
 
             # confirm TCU ID
-            if dcm_id != car.tcu_model:
-                return None
-
-            # confirm SIM ID
-            if car.iccid != sim_id:
+            if navi_id != car.tcu_serial:
                 return None
 
             # authenticate without user&pass for QY8XXX by detecting rss format

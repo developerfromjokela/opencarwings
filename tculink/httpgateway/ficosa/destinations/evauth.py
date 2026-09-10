35183aa29b749b2c47b741a1e047162bedcd56bc
Developer From Jokela <developerfromjokela@gmail.com>
2026-09-10 23:48:31 +0300

    Small fix to probe config payload generation

--- tculink/httpgateway/ficosa/destinations/evauth.py
+++ tculink/httpgateway/ficosa/destinations/evauth.py
@@ -57,13 +57,15 @@ def handle(_, acp_data: dict, car: Car, source_id: int, __) -> bytes:
             config_encoder = composer.ACPProbeConfigRaw()
             config_encoder.service_type = config_template["service_type"]
 
-            for field, info in config_template["fields"].items():
-                if field in config_payload:
-                    value = config_payload[field]
-                    field_type = info["type"]
-                    if field_type == ConfigurationFieldType.SELECT:
-                        config_bin = PROBE_DATACONFIG[config_template["service_type"]][value]
-                        config_encoder.records.extend(composer.parse_config_file_to_chunks(config_bin))
+            if config_payload["type"] == "send":
+                config_payload = config_payload["data"]
+                for field, info in config_template["fields"].items():
+                    if field in config_payload:
+                        value = config_payload[field]
+                        field_type = info["type"]
+                        if field_type == ConfigurationFieldType.SELECT:
+                            config_bin = PROBE_DATACONFIG[config_template["service_type"]][value]
+                            config_encoder.records.extend(composer.parse_config_file_to_chunks(config_bin))
 
             acp_msg += config_encoder.encode()
             logger.debug(f"<< ACPProbeConfigRaw Message: {acp_msg.hex()}")

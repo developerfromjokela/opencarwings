8ff803c1543012eed5b807d4f5cb7f54e59aa129
Developer From Jokela <developerfromjokela@gmail.com>
2026-09-12 02:06:41 +0300

    Fix calculation behind G forces

--- ui/templatetags/utils.py
+++ ui/templatetags/utils.py
@@ -6,5 +6,7 @@ register = template.Library()
 def fix_gforce(value):
     if value is not None and value > 65535:
         byte_value = value.to_bytes(3, byteorder="big", signed=False)
-        return int.from_bytes(byte_value[1:], byteorder="big", signed=False)
-    return value
+        return (int.from_bytes(byte_value[1:], byteorder="big", signed=False)) / 9.80665
+    elif value is not None and value > 0:
+        return value / 9.80665
+    return 0

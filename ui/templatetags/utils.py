a6488c3eabe893fca4883daf15ef726b7e110d1c
Developer From Jokela <developerfromjokela@gmail.com>
2026-09-12 01:51:49 +0300

    One more fix

--- ui/templatetags/utils.py
+++ ui/templatetags/utils.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 @register.filter
 def fix_gforce(value):
-    if value is not None and value > 20000:
+    print(value)
+    if value is not None and value > 65535:
         byte_value = value.to_bytes(3, byteorder="big", signed=False)
         return int.from_bytes(byte_value[1:], byteorder="big", signed=False)
     return value

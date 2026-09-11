9c2ac1b6f5d2bcc080d8d86148dd6c20c40549c1
Developer From Jokela <developerfromjokela@gmail.com>
2026-09-12 02:15:34 +0300

    one last div by 100

--- ui/templatetags/utils.py
+++ ui/templatetags/utils.py
@@ -6,7 +6,7 @@ register = template.Library()
 def fix_gforce(value):
     if value is not None and value > 65535:
         byte_value = int(value).to_bytes(3, byteorder="big", signed=False)
-        return (int.from_bytes(byte_value[1:], byteorder="big", signed=False)) / 9.80665
+        return ((int.from_bytes(byte_value[1:], byteorder="big", signed=False)) / 9.80665)/100.0
     elif value is not None and value > 0:
-        return int(value) / 9.80665
+        return int(value) / 100.0
     return 0

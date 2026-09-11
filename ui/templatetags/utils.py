1283e64dec64c5b84f840adeb940ef4a7dd39490
Developer From Jokela <developerfromjokela@gmail.com>
2026-09-12 02:15:52 +0300

    one last div by 100

--- ui/templatetags/utils.py
+++ ui/templatetags/utils.py
@@ -8,5 +8,5 @@ def fix_gforce(value):
         byte_value = int(value).to_bytes(3, byteorder="big", signed=False)
         return ((int.from_bytes(byte_value[1:], byteorder="big", signed=False)) / 9.80665)/100.0
     elif value is not None and value > 0:
-        return int(value) / 100.0
+        return int(value) / 10.0
     return 0
